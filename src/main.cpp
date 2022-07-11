715e1a352405cd3e48da0ae7d051c5f234ce721b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-11 16:37:27 +0100

    Add minimal build (#1)

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/nemu-cpp/debug-server/blob/main/LICENSE.txt
+*/
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
